4b49a0eb732c0734b13a1b74ad841282c96ed094
Rdduarte <rdduarte1811@gmail.com>
2024-10-03 01:45:47 -0300

    editing variables for auth

--- api/sendMail.js
+++ api/sendMail.js
@@ -16,8 +16,8 @@ app.post('/sendMail', async (req, res) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
+            user: EMAIL_USER,
+            pass: EMAIL_PASS
         }
     });
 
